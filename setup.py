43bd4ebf1688e7b5e82c2f00f579edcfe9a2165c
ikalyvas <jkalivas@gmail.com>
2022-01-03 21:04:36 +0200

    Adding dummy lines

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 version = '0.1.1'
 dependencies = ['requests==2.18.4']
 test_dependencies = ['nose==1.3.7', 'mock==2.0.0']
+
 setup_config = {'name': 'stackstats',
                 'version': version,
                 'description': 'A StackExchange API simple stats calculator',
@@ -15,8 +16,6 @@ setup_config = {'name': 'stackstats',
                     'console_scripts': ['stats=stackstats.stats_calc:main']
                 }
             }
-
-
 
 if __name__ == '__main__':
     setup(**setup_config)
